5d7af7d0e8a373b4d5457d11cd35c4b23b925ed1
Alex Kelly <alex.kelly@franklin.edu>
2020-08-19 15:26:08 -0400

    Added mysql varients of create statements...I think I'm doing this wrong :/

---
+++ shorty/sql/my_create_namespace_table.sql
@@ -0,0 +1,6 @@
+    -- :name my_create_namespace_table
+        CREATE TABLE namespace(
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        name varchar(25),
+        description varchar(255)
+        );
